43b04928d9b64485ba3274c6b05855b73448c68b
Jan Kuehle <jkuehle90@gmail.com>
2021-06-26 13:54:58 +0200

    Finder

--- curl-history.plugin.zsh
+++ curl-history.plugin.zsh
@@ -1,10 +1,26 @@
-actual_curl=$(command -v curl)
+actual_curl=$(command -pv curl)
 config_dir="$HOME/.config/curl-history"
 
 mkdir -p "$config_dir"
 
-curlh() {
+curl() {
 	file_name="$(date +%s) ${@//[^-a-zA-Z0-9.:_= ]/~}"
 	log_file="$config_dir/${file_name:0:50}.log"
-	$actual_curl "$@" | tee "$log_file"
+	echo -E "curl $@" >"$log_file"
+	echo "" >>"$log_file"
+	$actual_curl "$@" | tee -a "$log_file"
+}
+
+curlh() {
+	INITIAL_QUERY="$1"
+	RG_PREFIX="rg --files-with-matches --no-heading --smart-case "
+	(
+		cd "$config_dir"
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
+			fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+				--disabled \
+				--query "$INITIAL_QUERY" \
+				--layout=reverse \
+				--preview 'cat {}'
+	)
 }
